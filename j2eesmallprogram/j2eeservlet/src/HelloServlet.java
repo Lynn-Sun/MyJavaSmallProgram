53d46fd5f414cac456915ada47683c97d28c86b7
Lynn-Sun <15606456991@qq.com>
2018-08-17 21:56:10 +0800

    servlet实现跳转和随着服务器自启动。

--- j2eesmallprogram/j2eeservlet/src/HelloServlet.java
+++ j2eesmallprogram/j2eeservlet/src/HelloServlet.java
@@ -1,11 +1,21 @@
 import java.io.IOException;
 import java.util.Date;
 
+import javax.naming.InitialContext;
+import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet{
+
+	public HelloServlet() {
+		System.out.println("HelloServlet实例化");
+	}
+	
+	public void init(ServletConfig config){
+		System.out.println("init()进行初始化");
+	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response){
 		
--- j2eesmallprogram/j2eeservlet/src/LoginServlet.java
+++ j2eesmallprogram/j2eeservlet/src/LoginServlet.java
@@ -26,57 +26,29 @@ public class LoginServlet extends HttpServlet {
 		System.out.println("destroy()");
 	}
 
-	 @Override
-	 protected void service(HttpServletRequest request, HttpServletResponse
-	 response)
-	 throws ServletException, IOException {
-	
-	 // 使用UTF-8的编码和解码格式。
-	 request.setCharacterEncoding("UTF-8");
-	 String name = request.getParameter("name");
-	 String password = request.getParameter("password");
-	
-	 System.out.println("name:" + name);
-	 System.out.println("password:" + password);
-	
-	 String html = null;
-	 if ("admin".equals(name) && "admin".equals(password)) {
-	
-	 html = "<div style='color:green'>登录成功!!!</div>";
-	
-	 }
-	
-	 else
-	
-	 html = "<div style='color:red'>登录失败!!!</div>";
-	
-	 // 加上这句话，返回中文的响应。
-	 response.setContentType("text/html; charset=UTF-8");
-	 PrintWriter pw = response.getWriter();
-	 pw.println(html);
-	 }
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse
+			response)
+					throws ServletException, IOException {
 
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//			throws ServletException, IOException {
-//		String name = request.getParameter("name");
-//		String password = request.getParameter("password");
-//
-//		System.out.println("name:" + name);
-//		System.out.println("password:" + password);
-//		String html = null;
-//		PrintWriter pw = response.getWriter();
-//		if ("admin".equals(name) && "admin".equals(password)) {
-//			html = "<div style='color:green'>success</div>";
-//
-//		}
-//
-//		else {
-//
-//			html = "<div style = 'color:red'>fail</div>";
-//
-//		}
-//
-//		pw.println(html);
-//	}
+		// 使用UTF-8的编码和解码格式。
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+
+		System.out.println("name:" + name);
+		System.out.println("password:" + password);
+
+		//String html = null;
+		if ("admin".equals(name) && "admin".equals(password)) {
+
+			//服务器端跳转，浏览器地址不变。
+			//request.getRequestDispatcher("success.html").forward(request, response);
+			response.sendRedirect("success.html");
+		}
+
+		else
+			response.sendRedirect("fail.html");		
+	}
 
 }
